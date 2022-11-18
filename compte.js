4cce60a732bd8a4e6d07659facc7e962b498aed0
Oky-g <118482855+Oky-g@users.noreply.github.com>
2022-11-18 20:33:56 +0100

    MAJ css et finition des pages pour le Day 1
    css : ajustement des composent des différentes pages.

--- compte.js
+++ compte.js
@@ -6,16 +6,16 @@ contactForm.addEventListener("submit", function (e) {
   let messageName = [];
   let messageAddress = [];
   if (
-    (fullname.value == "Oky" && address.value == "9874")
+    (fullname.value == "Oky" && address.value == "9875")
   ) {
     sessionStorage.setItem("token", "connected");
     e.preventDefault();
-    window.location.assign("profil-modification-Oky.html");
+    window.location.assign("profil-modification_Oky.html");
   }
-  else if (fullname.value == "Okyg" && address.value == "9875") {
+  else if (fullname.value == "Oky" && address.value == "9874") {
     sessionStorage.setItem("token", "connected");
     e.preventDefault();
-    window.location.assign("Profil Oky.html");
+    window.location.assign("profil_Oky.html");
   }
 
   else if (fullname.value && address.value) {
